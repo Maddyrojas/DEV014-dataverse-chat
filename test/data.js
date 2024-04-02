924a5bb4f29d8a1b941e4e9efe6811b2060be5b5
Madelyn Rojas Moya <mmrojasmoya@gmail.com>
2024-04-02 14:02:11 -0600

    nueva rama

---
+++ test/data.js
@@ -0,0 +1,30 @@
+// Aqui puedes agregar los datos que necesites para tus pruebas
+// al menos debe tener uno que se llame "data"
+
+export const data = [
+    {// 01_Volcán Arenal
+      "id": "01_lugarTuristico",
+      "name": "Volcán Arenal",
+      "location": "Alajuela",
+      "shortDescription": "Impresionante volcán activo con vistas panorámicas.",
+      "tipoTurismo": "turismo de aventura",
+      "gastoPromedio": 150,
+    },
+    {//02_Parque Nacional Manuel Antonio
+      "id": "02_lugarTuristico",
+      "name": "Parque Nacional Manuel Antonio",
+      "location": "Puntarenas",
+      "shortDescription": "Reserva natural con playas prístinas y diversidad de vida silvestre.",
+      "tipoTurismo": "turismo de playa",
+      "gastoPromedio": 120,
+    },
+    {//07_Teatro Nacional de Costa Rica
+      "id": "07_lugarTuristico",
+      "name": "Teatro Nacional de Costa Rica",
+      "location": "San José",
+      "shortDescription": "Joyero arquitectónico y cultural en el corazón de San José.",
+      "tipoTurismo": "turismo cultural",
+      "gastoPromedio": 50,
+    },
+  ];
+  
