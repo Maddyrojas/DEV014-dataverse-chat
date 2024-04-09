099ef0011ba1df2e6e13ead65402626d65495f34
Madelyn Rojas Moya <mmrojasmoya@gmail.com>
2024-04-08 19:44:38 -0600

    div Chat Zone

--- src/views/IndividualChat.js
+++ src/views/IndividualChat.js
@@ -13,7 +13,7 @@ const divChatZone = mainChatElement.querySelector('div[id="chat-zone"]');
 
 function enterMessage(msj) {
   const messageElement = document.createElement('div');
-  messageElement.classList.add('message', type);
+  messageElement.classList.add("enterMessage");
   messageElement.innerHTML = `<strong>MADELYN:</strong> ${msj}`;
   divChatZone.appendChild(messageElement);
   //chatContainer.scrollTop = chatContainer.scrollHeight;
@@ -35,19 +35,22 @@ export function autoExpand(element) {
   }
 }
 
-export const IndividualChat = (objTour) => {
-  const individualChat = document.createElement("div");
+export const IndividualChat = (objName) => {
+  const individualChat = document.createElement("div"); //div all page
+  const divFirstMsj = document.createElement('div');
 
   const divInfoLaptop = mainChatElement.querySelector('div[id="infOnPC"]');
   const btnSendMsj = mainChatElement.querySelector('button[class="btn-sendMsj"]');
   const textArea = mainChatElement.querySelector("textArea");
 
-
-  const infoTour = data.find(tour => tour.name === objTour.name);
+  const infoTour = data.find(tour => tour.name === objName.name);
 
   divInfoLaptop.querySelector("h1").textContent = infoTour.name;
   divInfoLaptop.querySelector("img").src = infoTour.imageUrl;
 
+  divFirstMsj.classList.add("enterResponse");
+  divFirstMsj.innerHTML=`<strong>System:</strong> Hola yo soy ${infoTour.name}, te invito a conocerme por medio de tus consultas, por favor no te guardes ninguna duda...`;
+  divChatZone.appendChild(divFirstMsj);
 
   //----------EVENT----------//
   btnSendMsj.addEventListener('click', () => {
