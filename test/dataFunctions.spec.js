0b75ae34028188fb8960b13534adae6529684dce
Madelyn Rojas Moya <mmrojasmoya@gmail.com>
2024-04-10 22:28:26 -0600

    test

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,4 +1,4 @@
-import { filterData, sortData, computeStats } from '../lib/dataFunctions.js';
+import { filterData, sortData, computeStats } from '../src/lib/dataFunction.js';
 import { data as fakeData } from './data.js';
 
 
@@ -14,11 +14,11 @@ describe('test function `filterData`', () => {
   it('devuelve `filterData` con array de objetos key:value', () => {
     expect(filterData(fakeData, 'location', 'Alajuela')).toEqual(expectedFilter);
   });
-  
+
 });
 
 describe('test function `sortData`', () => {
-  const expectedSort = [{//02_Parque Nacional Manuel Antonio
+  const sortNameAsc = [{//02_Parque Nacional Manuel Antonio
     "id": "02_lugarTuristico",
     "name": "Parque Nacional Manuel Antonio",
     "location": "Puntarenas",
@@ -42,12 +42,95 @@ describe('test function `sortData`', () => {
     "tipoTurismo": "turismo de aventura",
     "gastoPromedio": 150,
   },];
+  const sortNameDesc = [{// 01_Volcán Arenal
+    "id": "01_lugarTuristico",
+    "name": "Volcán Arenal",
+    "location": "Alajuela",
+    "shortDescription": "Impresionante volcán activo con vistas panorámicas.",
+    "tipoTurismo": "turismo de aventura",
+    "gastoPromedio": 150,
+  },
+  {//07_Teatro Nacional de Costa Rica
+    "id": "07_lugarTuristico",
+    "name": "Teatro Nacional de Costa Rica",
+    "location": "San José",
+    "shortDescription": "Joyero arquitectónico y cultural en el corazón de San José.",
+    "tipoTurismo": "turismo cultural",
+    "gastoPromedio": 50,
+  },
+  {//02_Parque Nacional Manuel Antonio
+    "id": "02_lugarTuristico",
+    "name": "Parque Nacional Manuel Antonio",
+    "location": "Puntarenas",
+    "shortDescription": "Reserva natural con playas prístinas y diversidad de vida silvestre.",
+    "tipoTurismo": "turismo de playa",
+    "gastoPromedio": 120,
+  },
+  ];
+  const sortPriceDesc = [{// 01_Volcán Arenal
+    "id": "01_lugarTuristico",
+    "name": "Volcán Arenal",
+    "location": "Alajuela",
+    "shortDescription": "Impresionante volcán activo con vistas panorámicas.",
+    "tipoTurismo": "turismo de aventura",
+    "gastoPromedio": 150,
+  },
+  {//02_Parque Nacional Manuel Antonio
+    "id": "02_lugarTuristico",
+    "name": "Parque Nacional Manuel Antonio",
+    "location": "Puntarenas",
+    "shortDescription": "Reserva natural con playas prístinas y diversidad de vida silvestre.",
+    "tipoTurismo": "turismo de playa",
+    "gastoPromedio": 120,
+  },
+  {//07_Teatro Nacional de Costa Rica
+    "id": "07_lugarTuristico",
+    "name": "Teatro Nacional de Costa Rica",
+    "location": "San José",
+    "shortDescription": "Joyero arquitectónico y cultural en el corazón de San José.",
+    "tipoTurismo": "turismo cultural",
+    "gastoPromedio": 50,
+  },
+  ];
+  const sortPriceAsc = [{//07_Teatro Nacional de Costa Rica
+    "id": "07_lugarTuristico",
+    "name": "Teatro Nacional de Costa Rica",
+    "location": "San José",
+    "shortDescription": "Joyero arquitectónico y cultural en el corazón de San José.",
+    "tipoTurismo": "turismo cultural",
+    "gastoPromedio": 50,
+  },
+  {//02_Parque Nacional Manuel Antonio
+    "id": "02_lugarTuristico",
+    "name": "Parque Nacional Manuel Antonio",
+    "location": "Puntarenas",
+    "shortDescription": "Reserva natural con playas prístinas y diversidad de vida silvestre.",
+    "tipoTurismo": "turismo de playa",
+    "gastoPromedio": 120,
+  },
+  {// 01_Volcán Arenal
+    "id": "01_lugarTuristico",
+    "name": "Volcán Arenal",
+    "location": "Alajuela",
+    "shortDescription": "Impresionante volcán activo con vistas panorámicas.",
+    "tipoTurismo": "turismo de aventura",
+    "gastoPromedio": 150,
+  },];
   it('devuelve `SortOf` con un array ordenado de objetos key:value', () => {
-    expect(sortData(fakeData, 'name', 'asc')).toEqual(expectedSort);
+    expect(sortData(fakeData, 'name', 'asc')).toEqual(sortNameAsc);
+  });
+  it('sortNameDesc', () => {
+    expect(sortData(fakeData, 'name', 'desc')).toEqual(sortNameDesc);
+  });
+  it('sortPriceAsc', () => {
+    expect(sortData(fakeData, 'gastoPromedio', 'asc')).toEqual(sortPriceAsc);
+  });
+  it('sortPriceDesc', () => {
+    expect(sortData(fakeData, 'gastoPromedio', 'desc')).toEqual(sortPriceDesc);
   });
 });
 
-describe('test function `computeStats`', () =>{
+describe('test function `computeStats`', () => {
   const expectCompute = [{
     "tipoTurismo": "turismo de playa",
     "cantidad": 1,
--- test/openAIApi.spec.js
+++ test/openAIApi.spec.js
@@ -1,9 +1,39 @@
-import { communicateWithOpenAI } from '../src/utils/openAIApi.js';
+import { communicateWithOpenAI } from "../src/lib/openAIApi.js";
+
+window.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve("¡Hola! ¡Bienvenido a Isla Tortuga! ¿En qué puedo ayudarte hoy?"),
+  })
+);
 
 describe('communicateWithOpenAI', () => {
+  beforeEach(() => {
+    fetch.mockClear();
+  });
   test('communicateWithOpenAI', () => {
-    return communicateWithOpenAI().then(data => {
-      expect(data).toBe('example');
-    });
+    const prompt = "Hola";
+    const tour = {//11_Isla Tortuga
+      "id": "11_lugarTuristico",
+      "name": "Isla Tortuga",
+      "location": "Puntarenas",
+      "shortDescription": "Isla paradisíaca con aguas cristalinas y zonas acuáticas.",
+      "description": "Isla Tortuga es un refugio tropical con playas de arena blanca y aguas cristalinas, explora la vida marina en un entorno paradisíaco y vive la emoción de practicar deportes acuáticos en un escenario único. Relájate bajo el sol, es un destino perfecto para quienes buscan escapar del bullicio y disfrutar de la tranquilidad del mar. Relájate bajo el sol, toma un baño refrescante en las aguas cristalinas y disfruta de la paz y la belleza natural que te rodea. Un destino ideal para escapar y disfrutar de la belleza natural en el Golfo de Nicoya.",
+      "imageUrl": "https://raw.githubusercontent.com/Maddyrojas/DEV014-Dataverse/main/Imagenes%20Dataverse/11_lugarTuristico.jpg",
+      "tipoTurismo": "turismo de playa",
+      "gastoPromedio": 120,
+      "facts": {
+        "museo": "undefined",
+        "compras": "Tienda de Recuerdos Isla Tortuga",
+        "alimentacion": "Restaurante Tropical"
+      }
+    }
+    return communicateWithOpenAI(tour, prompt)
+      .then(response => {
+        expect(response).toBe('¡Hola! ¡Bienvenido a Isla Tortuga! ¿En qué puedo ayudarte hoy?');
+      })
+      .catch(error =>{
+        expect(error).toBe(error);
+      });
   });
 });
